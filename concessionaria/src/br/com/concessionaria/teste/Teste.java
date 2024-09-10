52e16a12af6a45e91904c26329b5e67365b33460
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-10 17:26:34 -0300

    Herança,polimorfismo, estruturas de decisão, repetição e tratamento de erros.

--- concessionaria/src/br/com/concessionaria/teste/Teste.java
+++ concessionaria/src/br/com/concessionaria/teste/Teste.java
@@ -8,42 +8,47 @@ import br.com.concessionaria.showroom.Carro;
 public class Teste {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
+
+		String venda = "Vendas";
+		String financeiro = "Financeiro";
+		String sac = "SAC";
+
+		boolean key = true;
+
 		
-		Carro carro = new Carro();
-		
-		//Crie entradas para cada atributo da classe carro:
-		//Respeite o tipo de dados
-		System.out.println("Digite o nome do carro!");
-		carro.setNome(scan.next());
-		
-		System.out.println("Digite a cor do carro!");
-		carro.setCor(scan.next());
-		
-		System.out.println("Digite o preço do carro!");
-		carro.setPreco( Double.parseDouble(scan.next()) );
-		
-		System.out.println("Digite o modelo do carro!");
-		carro.setModelo(scan.next());
-		
-		System.out.println("Digite o ano do carro!");
-		carro.setAno(Integer.parseInt(scan.next()));
-		//Volte na classe carro e rie 
-		// um método que imprima todos os atributos da classe;
-		
-		//Chame o novo método para verificar o resultado:
-		carro.imprimir();
-		scan.close();
-		
+
+			while (key) {
+				System.out.println("Digite apenas umas das opções abaixo:");
+				System.out.println("1 - VENDAS\n2 - FINANCEIRO\n3 - SAC");
+				int opcao = 0;
+				
+				try {
+					opcao = Integer.parseInt(scan.next());
+				} catch (NumberFormatException e) {
+					System.out.println("SOMENTE NÚMEROS PODEM SER DIGITADOS!");
+					opcao = Integer.parseInt(scan.next());
+				}
+				
+				if (opcao == 1) {
+					System.out.println("A opção escolhida foi : " + venda);
+					key = false;
+				} else if (opcao == 2) {
+					System.out.println("A opção escolhida foi : " + financeiro);
+					key = false;
+				} else if (opcao == 3) {
+					System.out.println("A opção escolhida foi : " + sac);
+					key = false;
+				} else {
+					System.out.println("Digite uma opção válida!");
+				}
+
+			}
+
 	
-		Vendedor vendedor = new Vendedor();
-		
-		
-		
-		
-		
-		
+
+		scan.close();
 	}
 
 }
