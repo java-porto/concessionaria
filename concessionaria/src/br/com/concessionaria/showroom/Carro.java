1f06870b410bc4cc17c324ef50ada86a9ca52a8d
feliperodalv <felipe.rodrigues.alves10@gmail.com>
2024-09-10 15:26:33 -0300

    Parseamento, Encapsulamento

--- concessionaria/src/br/com/concessionaria/showroom/Carro.java
+++ concessionaria/src/br/com/concessionaria/showroom/Carro.java
@@ -1,17 +1,17 @@
 package br.com.concessionaria.showroom;
 
 public class Carro {
-	
+
 	private String nome;
 	private String cor;
 	private double preco;
 	private String modelo;
 	private int ano;
-	
+
 	public Carro() {
-		
+
 	}
-	
+
 	public Carro(String nome, String cor, double preco, String modelo, int ano) {
 		this.nome = nome;
 		this.cor = cor;
@@ -19,48 +19,50 @@ public class Carro {
 		this.modelo = modelo;
 		this.ano = ano;
 	}
-	
+
 	public String getNome() {
 		return this.nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getCor() {
 		return this.cor;
 	}
-	
+
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
-	
+
 	public double getPreco() {
 		return this.preco;
 	}
-	
-	public void setNome(double preco) {
+
+	public void setPreco(double preco) {
 		this.preco = preco;
 	}
-	
+
 	public String getModelo() {
 		return this.modelo;
 	}
-	
-	public void setModelo (String modelo) {
+
+	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
-	
+
 	public int getAno() {
 		return this.ano;
 	}
-	
+
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
-	
 
+	// Método para imprimir todos os atributos
+	public void imprimirAtributos() {
+		System.out.println("Nome: " + this.nome + " Cor: " + this.cor + " Preço: R$" + this.preco + " Modelo: " + this.modelo + " Ano: " + this.ano + ".\nParabéns!");
 
-	
+	}
 }
--- concessionaria/src/br/com/concessionaria/teste/Teste.java
+++ concessionaria/src/br/com/concessionaria/teste/Teste.java
@@ -18,17 +18,49 @@ public class Teste {
 		
 		//System.out.println("O nome do carro é: " + carro);
 
-		String dados = " "; 
+		//String dados = " "; 
 		
-		Scanner sc;
-		sc = new Scanner(System.in);
+		//Scanner sc;
+		//sc = new Scanner(System.in);
 		
-		System.out.println("Digite algo: ");
-		dados = sc.nextLine();
+		//System.out.println("Digite algo: ");
+		//dados = sc.nextLine();
 		
-		System.out.println("= " + dados);
+		//System.out.println("= " + dados);
 		
-		sc.close();
+		//sc.close();
+		
+		
+		//AULA - 10/09/2024
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Carro carro = new Carro();
+		
+		
+		System.out.println("Digite o nome do carro: ");	
+		carro.setNome(scan.next());
+		
+		System.out.println("Digite a cor do carro: ");	
+		carro.setCor(scan.next());
+		
+		System.out.println("Digite o preço do carro: ");	
+		carro.setPreco(Double.parseDouble(scan.next()));
+
+		System.out.println("Digite o modelo do carro: ");	
+		carro.setModelo(scan.next());
+		
+		System.out.println("Digite o ano do carro: ");	
+		carro.setAno(Integer.parseInt(scan.next()));
+		
+		//Volte na classe carro e crie um metódo
+		//que imprima todos os atributos da classe carro;
+		//Chame o novo método para verificar o resultado:
+		
+		carro.imprimirAtributos();
+
+		
+		scan.close();
 	}
 
 }
