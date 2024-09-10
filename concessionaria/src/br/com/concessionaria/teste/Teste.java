98c8b4ef18d837f78e48cafc8358fa9a3848812c
gamandu <gabrel.apaps@gmail.com>
2024-09-10 15:26:24 -0300

    Parseamento, Encapsulamento

--- concessionaria/src/br/com/concessionaria/teste/Teste.java
+++ concessionaria/src/br/com/concessionaria/teste/Teste.java
@@ -22,25 +22,57 @@ public class Teste {
 //		
 		
 		//Instanciando o scanner
-		Scanner scan;
+//		Scanner scan;
 		
 		//contruindo o objeto Scanner
-		scan = new Scanner(System.in);
+//		scan = new Scanner(System.in);
 		
 		//Recebendo dados via console e colocando em uma variável:
-		String dados = "";
+//		String dados = "";
 		
-		System.out.println("digite algo neste terminal! :");
-		dados = scan.nextLine();
+	//	System.out.println("digite algo neste terminal! :");
+	//	dados = scan.nextLine();
 		
-		System.out.println("este foi o valor digitado : " + dados);
-		
-		
+//		System.out.println("este foi o valor digitado : " + dados);
 		
 		
+		
+		
+//		scan.close();
+		
+//=====================================================================================================================================		
+		//Crie entradas para cada atributo da classe carro:
+		//Respeite o tipo de dados
+		Scanner scan = new Scanner(System.in);
+		
+		Carro carro = new Carro("Civic", "Prata", 15.000, "LX", 2000);
+		
+		System.out.println("Digite o nome do carro!");
+		carro.setNome(scan.next());
+		
+		System.out.println("Digite a cor carro!");
+		carro.setCor(scan.next());
+		
+		System.out.println("Digite o preço do carro!");
+		carro.setPreco( Double.parseDouble(scan.next()) );
+		
+		System.out.println("Digite o modelo do carro!");
+		carro.setModelo(scan.next());
+		
+		System.out.println("Digite o ano do carro!");
+		carro.setAno( Integer.parseInt( scan.next()) );
+		//Volte na classe carro e crie um método que imprima todos os atributos da classe;
+		
+		//Chame o novo método para verificar o resultado:
+		
+	
+	
+		carro.imprimir();
 		scan.close();
-		
-		
+	
+	
+	
 	}
+	
 
 }
