da7f1fef4383f7c52314be4253e8cdbd186541d9
gamandu <gabrel.apaps@gmail.com>
2024-09-10 17:22:27 -0300

    Herança, Polimorfismo, Estruturas de decisão, repetição e tratamento de erros.

--- concessionaria/src/br/com/concessionaria/teste/Teste.java
+++ concessionaria/src/br/com/concessionaria/teste/Teste.java
@@ -7,71 +7,48 @@ import br.com.concessionaria.showroom.Carro;
 public class Teste {
 
 	public static void main(String[] args) {
-		
-//		//1 - Instanciar a classe carro:
-//		
-//		//2 - Construir o objeto carro:
-//		
-//		//3 - Imprimir o nome do carro :
-//		
-//		Carro carro;
-//		
-//		carro = new Carro("Civic", "Prata", 15.000, "LX", 2000);
-//		
-//		System.out.println("Carro setado com o nome de : " + carro.getNome());
-//		
-		
-		//Instanciando o scanner
-//		Scanner scan;
-		
-		//contruindo o objeto Scanner
-//		scan = new Scanner(System.in);
-		
-		//Recebendo dados via console e colocando em uma variável:
-//		String dados = "";
-		
-	//	System.out.println("digite algo neste terminal! :");
-	//	dados = scan.nextLine();
-		
-//		System.out.println("este foi o valor digitado : " + dados);
-		
-		
-		
-		
-//		scan.close();
-		
-//=====================================================================================================================================		
-		//Crie entradas para cada atributo da classe carro:
-		//Respeite o tipo de dados
-		Scanner scan = new Scanner(System.in);
-		
-		Carro carro = new Carro("Civic", "Prata", 15.000, "LX", 2000);
-		
-		System.out.println("Digite o nome do carro!");
-		carro.setNome(scan.next());
-		
-		System.out.println("Digite a cor carro!");
-		carro.setCor(scan.next());
-		
-		System.out.println("Digite o preço do carro!");
-		carro.setPreco( Double.parseDouble(scan.next()) );
-		
-		System.out.println("Digite o modelo do carro!");
-		carro.setModelo(scan.next());
-		
-		System.out.println("Digite o ano do carro!");
-		carro.setAno( Integer.parseInt( scan.next()) );
-		//Volte na classe carro e crie um método que imprima todos os atributos da classe;
-		
-		//Chame o novo método para verificar o resultado:
-		
 	
+	Scanner scan = new Scanner(System.in);
+		
+	String venda = "Vendas";
+	String financeiro = "Financeiro";
+	String sac = "SAC";
 	
-		carro.imprimir();
-		scan.close();
+	boolean key = true;
 	
 	
 	
+	
+	while (key) {
+		System.out.println("Digite apenas uma das opções abaixo:");
+		System.out.println("1 - VENDAS\n2 - FINANCEIRO\n3 - SAC");
+		int opcao = 0;
+		
+		try {
+			opcao = Integer.parseInt(scan.next());
+		}  catch (NumberFormatException e) {
+			System.out.println("Somente numero podem ser digitados!");
+			opcao = Integer.parseInt(scan.next());
+		}
+		if(opcao == 1) {
+			System.out.println("A opção escolhida foi : " + venda);
+		key = false;
+		}else if(opcao == 2) {
+			System.out.println("A opção escolhida foi : " + financeiro);
+		key = false;
+		}else if(opcao == 3) {
+			System.out.println("A opção escolhida foi : " + sac);
+		key = false;
+		}else {
+			System.out.println("Digite uma opção valida!");
+		}
+		
+	}
+	
+	
+
+	
+	scan.close();
 	}
 	
 
