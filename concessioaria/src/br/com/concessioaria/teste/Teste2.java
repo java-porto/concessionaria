e118c9f5a8499ce433cbcdbcb313cf4171c76331
JorgeClaudino <jorge.babarovic.claudino@gmail.com>
2024-09-17 17:27:44 -0300

    Treinamento de erros e Arrays e forEach. Switch Case.

---
+++ concessioaria/src/br/com/concessioaria/teste/Teste2.java
@@ -0,0 +1,30 @@
+package br.com.concessioaria.teste;
+
+import br.com.concessioaria.showroom.Carro;
+
+public class Teste2 {
+
+public static void main(String[] args) {
+	
+	Mecanico mecanico [] = new Mecanico [5];
+	mecanico [0] = new Mecanico();
+	mecanico [1] = new Mecanico();
+	mecanico [2] = new Mecanico();
+	mecanico [3] = new Mecanico();
+	mecanico [4] = new Mecanico();
+	mecanico [5] = new Mecanico();
+
+	mecanico [0].setNome("Jonas");
+	mecanico [1].setNome("Gabiru");
+	mecanico [2].setNome("");
+	mecanico [3].setNome("");
+	mecanico [4].setNome("");
+	mecanico [5].setNome("");
+}
+
+	
+	
+
+	
+	
+}
