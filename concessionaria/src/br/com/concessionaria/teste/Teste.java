9dbad30175476c3af40affa6cd51b5a3698f07bb
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-17 17:21:30 -0300

    Tratamento de erros, Arrays e forEach. Switch case.

--- concessionaria/src/br/com/concessionaria/teste/Teste.java
+++ concessionaria/src/br/com/concessionaria/teste/Teste.java
@@ -17,26 +17,45 @@ public class Teste {
 
 		boolean key = true;
 
-		
+		comando:
 
-			while (key) {
-				System.out.println("Digite apenas umas das opções abaixo:");
-				System.out.println("1 - VENDAS\n2 - FINANCEIRO\n3 - SAC");
-				int opcao = 0;
-				
-				try {
-					opcao = Integer.parseInt(scan.next());
-				} catch (NumberFormatException e) {
-					System.out.println("SOMENTE NÚMEROS PODEM SER DIGITADOS!");
-					opcao = Integer.parseInt(scan.next());
-				}
-				
+		while (key) {
+			System.out.println("Selecione apenas umas das opções abaixo:");
+			System.out.println("1 - VENDAS\n2 - FINANCEIRO\n3 - SAC");
+
+			int opcao = 0;
+			
+		
+			try {
+
+				opcao = Integer.parseInt(scan.next());
+
 				if (opcao == 1) {
-					System.out.println("A opção escolhida foi : " + venda);
-					key = false;
+					System.out.println("Selecione apenas umas das opções abaixo:");
+					System.out.println("1 - Comprar\n2 - Vender\n3 - Representante");
+					opcao = Integer.parseInt(scan.next());
+					
+					switch (opcao) {
+					case 1:
+						System.out.println("Opção escolhida COMPRAR");
+						key = false;
+						break;
+					case 2:
+						System.out.println("Opção escolhida VENDER");
+						key = false;
+						break;
+					case 3:
+						System.out.println("Opção escolhida REPRESENTANTE");
+						key = false;
+						break;
+					default:
+						System.out.println("Somentes opções entre 1 e 3 são permitidas.");
+						break;
+					}
+
 				} else if (opcao == 2) {
 					System.out.println("A opção escolhida foi : " + financeiro);
-					key = false;
+
 				} else if (opcao == 3) {
 					System.out.println("A opção escolhida foi : " + sac);
 					key = false;
@@ -44,11 +63,14 @@ public class Teste {
 					System.out.println("Digite uma opção válida!");
 				}
 
+			} catch (NumberFormatException e) {
+				System.out.println("SOMENTE NÚMEROS PODEM SER DIGITADOS!");
+				continue comando;
 			}
 
-	
+		}//final while
 
 		scan.close();
-	}
+	}//Final da main
 
-}
+}//Final da class
