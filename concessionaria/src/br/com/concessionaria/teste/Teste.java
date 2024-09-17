40088f5b7f0e57c3fe2b7d9252ac4fcd1b3559a6
feliperodalv <felipe.rodrigues.alves10@gmail.com>
2024-09-17 17:21:55 -0300

    Tratamento de Erros, Arrays e forEach. Switch case.

--- concessionaria/src/br/com/concessionaria/teste/Teste.java
+++ concessionaria/src/br/com/concessionaria/teste/Teste.java
@@ -32,7 +32,7 @@ public class Teste {
 		//sc.close();
 		
 		
-		//AULA - 10/09/2024
+		//AULA - 05/09/2024
 		
 		//Scanner scan = new Scanner(System.in);
 		
@@ -63,6 +63,8 @@ public class Teste {
 		
 		//Vendedor vendedor = new Vendedor();
 		
+		//Aula 10/09/2024
+		
 		Scanner scan = new Scanner(System.in);
 		
 		String venda = "Vendas";
@@ -71,20 +73,39 @@ public class Teste {
 		
 		boolean key = true;
 		
-
+		comando:
 		while(key) {
 			System.out.println("Digite apenas uma das opções abaixo: ");
 			System.out.println("1 - VENDAS\n2 - FINANCEIRO\n3 - SAC");
+			
 			int opcao = 0;
+			
+			
 			try {
 			    opcao = Integer.parseInt(scan.next());
-			} catch (NumberFormatException e) {
-				System.out.println("Somente números podem ser digitados!!!");
-				opcao = Integer.parseInt(scan.next());
-			}
+			
 			if(opcao == 1) {
-				System.out.println("A opção escolhida foi: " + venda);
-				key = false;
+				System.out.println("Selecione apenas uma das opções abaixo: ");
+				System.out.println("1 - Comprar Carro \n2 - Vender\n3 - Representante ");
+				opcao = Integer.parseInt(scan.next());
+				
+				switch(opcao) {
+				case 1:
+					System.out.println("opção escolhida COMPRAR");
+					key = false;
+					break;
+				case 2:
+					System.out.println("opção escolhida VENDER");
+					key = false;
+					break;
+				case 3:
+					System.out.println("opção escolhida REPRESENTANTE");
+					key = false;
+					break;
+				default:
+					System.out.println("Somente opções entre 1, 2 e 3 são permitidas.");
+					break;
+				}
 			}
 			else if (opcao == 2) {
 				System.out.println("A opção escolhida foi: " + financeiro);
@@ -95,10 +116,18 @@ public class Teste {
 				key = false;
 			}
 			else {
-				System.out.println("Digite ums opção válida!");
+				System.out.println("Digite uma opção válida!");
+			  }
+			}  catch (NumberFormatException e) {
+				System.out.println("Somente números podem ser digitados!!!");
+				continue comando;
 			}
-		}
+		} //Final While
 			
 		scan.close();
-		}
-}
+		
+		
+		//-- Aula 17/09/2024--
+		
+		} //Final da Main
+}  //Final da CLASS
---
+++ concessionaria/src/br/com/concessionaria/teste/Teste2.java
@@ -0,0 +1,34 @@
+package br.com.concessionaria.teste;
+
+import br.com.concessionaria.deptocomercial.Mecanico;
+import br.com.concessionaria.showroom.Carro;
+
+public class Teste2 {
+
+	public Teste2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+			
+			
+		Mecanico[] mecanico = new Mecanico[5];
+		mecanico[0] = new Mecanico();
+		mecanico[1] = new Mecanico();
+		mecanico[2] = new Mecanico();
+		mecanico[3] = new Mecanico();
+		mecanico[4] = new Mecanico();
+		
+		mecanico[0].setNome("Jonathan");
+		mecanico[1].setNome("Felipe");
+		mecanico[2].setNome("Vinicius");
+		mecanico[3].setNome("Jorge");
+		mecanico[4].setNome("Gabriel");
+		
+		for (Mecanico mecanicos : mecanico) {
+			System.out.println("Nome dos mêcanicos: " + mecanicos.getNome());
+		}
+	}
+
+}
