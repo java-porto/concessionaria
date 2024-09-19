10116afc56b2a23ee2b29099f48ab0e440c96636
aljoao <jv.feitosa2006@gmail.com>
2024-09-19 16:50:17 -0300

    Lista/showroom

--- concessionaria/src/br/com/concessionaria/dptocomercial/Vendedor.java
+++ concessionaria/src/br/com/concessionaria/dptocomercial/Vendedor.java
@@ -1,9 +1,127 @@
 package br.com.concessionaria.dptocomercial;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.concessionaria.showroom.Carro;
+
 public class Vendedor extends Comercial {
+	
+	private List<Carro> lista = null;
 
 	public Vendedor() {
+		
+		
+		
+		
+	lista = new ArrayList<Carro>();
+		
+		Carro carro = new Carro();
+		Mecanico mecanico = new Mecanico();
+		
+		
+		carro.setAno(2024);
+		carro.setCor("Branco");
+		carro.setNome("Creta");
+		carro.setPreco(100.000);
+		carro.setModelo("SUV");
+		mecanico.setId(1234);
+		mecanico.setNome("Jonas");
+		mecanico.setCargo("Mecânico 1");
+		carro.setMecanico(mecanico);
+		lista.add(carro);
+		
+		//
+		
+		carro = new Carro();
+	    mecanico = new Mecanico();
+	   
+		
+		carro.setAno(2025);
+		carro.setCor("Prata");
+		carro.setNome("Jeep");
+		carro.setPreco(125.000);
+		carro.setModelo("SUV");
+		mecanico.setId(2312);
+		mecanico.setNome("Gabiru");
+		mecanico.setCargo("Mecânico 2");
+		carro.setMecanico(mecanico);
+		lista.add(carro);
+		
+		
+		carro = new Carro();
+	    mecanico = new Mecanico();
+	    
+		
+		carro.setAno(2023);
+		carro.setCor("Vermelho");
+		carro.setNome("Tesla");
+		carro.setPreco(225.000);
+		carro.setModelo("SEDÃ");
+		mecanico.setId(3342);
+		mecanico.setNome("Jeniffer");
+		mecanico.setCargo("Mecânico 3");
+		carro.setMecanico(mecanico);
+		lista.add(carro);
+		
+		
+		carro = new Carro();
+	    mecanico = new Mecanico();
+	    
+		
+		carro.setAno(1967);
+		carro.setCor("Preto");
+		carro.setNome("Impala");
+		carro.setPreco(500.000);
+		carro.setModelo("SEDÃ");
+		mecanico.setId(9696);
+		mecanico.setNome("Johnson");
+		mecanico.setCargo("Mecânico 4");
+		carro.setMecanico(mecanico);
+		lista.add(carro);
+		
+		carro = new Carro();
+	    mecanico = new Mecanico();
+	   
+		
+		carro.setAno(1967);
+		carro.setCor("Preto");
+		carro.setNome("Mustang");
+		carro.setPreco(600.000);
+		carro.setModelo("Esportivo");
+		mecanico.setId(6904);
+		mecanico.setNome("Zóio");
+		mecanico.setCargo("Mecânico 5");
+		carro.setMecanico(mecanico);
+		lista.add(carro);
+		
+		
+		
+		
+		
+		
 	
 	}
+	
+       public void getLista() {
+		
+		for (Carro carro : lista) {
+			
+		
+			System.out.println("***SHOW ROOM***");
+			System.out.println("*******************************");	
+			System.out.println("NOME : " + carro.getNome());
+			System.out.println("*******************************");
+			System.out.println("ANO DE FABRICAÇÃO : " + carro.getAno());
+			System.out.println("*******************************");
+			System.out.println("MODELO : " + carro.getModelo());
+			System.out.println("*******************************");
+			System.out.println("COR : " + carro.getCor());
+			System.out.println("*******************************");
+			
+			
+		}
 
+       }
+       
 }
--- concessionaria/src/br/com/concessionaria/teste/teste2.java
+++ concessionaria/src/br/com/concessionaria/teste/teste2.java
@@ -1,6 +1,7 @@
 package br.com.concessionaria.teste;
 
 import br.com.concessionaria.dptocomercial.Mecanico;
+import br.com.concessionaria.dptocomercial.Vendedor;
 import br.com.concessionaria.showroom.Carro;
 
 public class teste2 {
@@ -11,75 +12,80 @@ public class teste2 {
 			
 	public static void main(String[] args) {
 				
-	Carro carro[] = new Carro[3];
-	carro[0] = new Carro();
-	carro[1] = new Carro();
-	carro[2] = new Carro();
+	//Carro carro[] = new Carro[3];
+	//carro[0] = new Carro();
+	//carro[1] = new Carro();
+	//carro[2] = new Carro();
 	
-	carro[0].setNome("Mavecka");
-	carro[1].setNome("Opala");
-	carro[2].setNome("Jeep");
+	//carro[0].setNome("Mavecka");
+	//carro[1].setNome("Opala");
+	//carro[2].setNome("Jeep");
 		
-	for (Carro carrinho : carro) {
-		System.out.println("Nome do carro : " + carrinho.getNome());
+	//for (Carro carrinho : carro) {
+		//System.out.println("Nome do carro : " + carrinho.getNome());
 		
-	}
+	//}
 		
 	//Exercício:
 	//crie um array de mecânicos com 5 mecanicos e imprima através do foreach
 	
-	Mecanico mecanico[] = new Mecanico [5];
-	mecanico[0] = new Mecanico();
-	mecanico[1] = new Mecanico();
-	mecanico[2] = new Mecanico();
-	mecanico[3] = new Mecanico();
-	mecanico[4] = new Mecanico();
+	//Mecanico mecanico[] = new Mecanico [5];
+	//mecanico[0] = new Mecanico();
+	//mecanico[1] = new Mecanico();
+	//qmecanico[2] = new Mecanico();
+	//mecanico[3] = new Mecanico();
+	//mecanico[4] = new Mecanico();
 	
-	mecanico[0].setNome("Arrascaeta");
-	mecanico[1].setNome("Datena");
-	mecanico[2].setNome("Kety <3");
-	mecanico[3].setNome("Pablo marçal");
-	mecanico[4].setNome("Joca");
+	//mecanico[0].setNome("Arrascaeta");
+	//mecanico[1].setNome("Datena");
+	//mecanico[2].setNome("Kety <3");
+	//mecanico[3].setNome("Pablo marçal");
+	//mecanico[4].setNome("Joca");
 	
-	for (Mecanico mecaninho : mecanico) {
-		System.out.println("Nome do mecânico : " + mecaninho.getNome());
-	}
+	//for (Mecanico mecaninho : mecanico) {
+		//System.out.println("Nome do mecânico : " + mecaninho.getNome());
+	//}
 	
-	int nrLinhas = 21;
-	int nrColunas = 4;
+	//int nrLinhas = 21;
+	//int nrColunas = 4;
 	
-	String[][] assentos = new String[nrLinhas][nrColunas];
+	//String[][] assentos = new String[nrLinhas][nrColunas];
 	
-	for (int x = 0; x < assentos.length; x++) {
+	//for (int x = 0; x < assentos.length; x++) {
 		
-		for (int i = 0; i < nrColunas; i++) {
-			assentos[x][i] = "O";
-		}
+		//for (int i = 0; i < nrColunas; i++) {
+			//assentos[x][i] = "O";
+		//}
 		
 		
 		
-	}
+	//}
 	
-for (int x = 0; x < assentos.length; x++) {
+//for (int x = 0; x < assentos.length; x++) {
 		
-		for (int i = 0; i < nrColunas; i++) {
-			if(i == 2) {
-				System.out.print("-");
-			}
-			System.out.print("["+ assentos[x][i] +"]");
+		//for (int i = 0; i < nrColunas; i++) {
+			//if(i == 2) {
+				//System.out.print("-");
+			//}
+			//System.out.print("["+ assentos[x][i] +"]");
 			
-		}
-		System.out.println("");
 		
-		
-	}
-	
-	
-	}
-	
+     //System.out.println("");
+
+    Vendedor vendedor = new Vendedor();
+    vendedor.getLista();
 	
 
+
+	
+	//}
+	
+	
+	//}
+
 	}
+	
+}
 
 
 
