6ccae3860171e4c933ba7978ba9718db4e1180aa
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-01 15:20:55 -0300

    refactor: Alterando assinatura dos métodos na classe DAO.

--- concessionaria/src/br/com/concessionaria/dao/CarroDAO.java
+++ concessionaria/src/br/com/concessionaria/dao/CarroDAO.java
@@ -12,6 +12,8 @@ public class CarroDAO {
 	public static List<Carro> lista = null;
 	
 	public CarroDAO() {
+		
+		if(lista == null) {
 		lista = new ArrayList<Carro>();
 
 		Carro carro = new Carro();
@@ -125,47 +127,25 @@ public class CarroDAO {
 		mecanico.setCargo("Mecanico 1");
 		carro.setMecanico(mecanico);
 		lista.add(carro);
-
-	}
-	
-	public void getLista(Vendedor vendedor) {
-		System.out.println("++++++++SHOW ROOM+++++");
-		for (int x = 0; x < lista.size() ; x++) {
-			
-			System.out.println("*********CARRO: "+ (lista.get(x).getNome()) +" ****************");
-			System.out.println("ANO FABRICAÇÃO : " + lista.get(x).getAno());
-			System.out.println("MODELO : " + lista.get(x).getModelo());
-			System.out.println("COR : " + lista.get(x).getCor());
-			System.out.println("PREÇO : " + lista.get(x).getPreco());
-			System.out.println("VENDEDOR RESPONSÁVEL : " + vendedor.getNome());
-			System.out.println("***********************************");
 		}
-
+	}
+	
+	public List<Carro> select() {
+		return lista;
 	}
 	
 	
-	public int getCarroByName(String nomeCarro) {
+	public Carro select(int id) {
 		for(int x = 0; x < lista.size(); x++) {
-			if(lista.get(x).getNome().equals(nomeCarro)) {
-				return lista.get(x).getId();
-			}
-		}
-		
-		return 0;
-	}
-	
-	//Crie um método que utilize o me´todo getCarroByName para retornar um Objeto Carro
-	// Através do ID.
-	public Carro getCarroById(int id) {
-		for(int x = 0; x < lista.size(); x++) {
-			if(lista.get(x).getId() == id) {
+			if(lista.get(x).getId() == id ) {
 				return lista.get(x);
 			}
 		}
+		
 		return null;
 	}
-	
-	public boolean removerCarroDaLista(Carro carro) {
+		
+	public boolean delete(Carro carro) {
 		for(int x = 0; x < lista.size(); x++) {
 			if(lista.get(x) == carro) {
 				return lista.remove(carro);
@@ -174,7 +154,7 @@ public class CarroDAO {
 		return false;
 	}
 	
-	public boolean insertCarro(Carro carro) {
+	public boolean insert(Carro carro) {
 		
 		int idCarro = lista.get(lista.size()-1).getId()+1;
 		carro.setId(idCarro);
@@ -184,5 +164,4 @@ public class CarroDAO {
 		return false;
 	}
 
-	
 }
