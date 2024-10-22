c8976552b4da069fb2537add017627c255063495
Jonathan-Pinheiro <jonathan-pontes@outlook.com>
2024-10-22 17:32:26 -0300

    Criação da classe ConnectionFactory

--- Loja01/src/main/java/br/com/concessionaria/teste/Teste.java
+++ Loja01/src/main/java/br/com/concessionaria/teste/Teste.java
@@ -1,16 +1,20 @@
 package br.com.concessionaria.teste;
 
 import br.com.concessionaria.controller.CarroDAO;
+import br.com.concessionaria.factory.ConnectionFactory;
 import br.com.concessionaria.model.Carro;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		CarroDAO cDAO = new CarroDAO();
-		Carro carro = cDAO.select(3);
+//		CarroDAO cDAO = new CarroDAO();
+//		Carro carro = cDAO.select(3);
+//		
+//		System.out.println("nome do carro: " + carro.getNome());
+//		System.out.println("Modelo do carro: " + carro.getModelo());
 		
-		System.out.println("nome do carro: " + carro.getNome());
-		System.out.println("Modelo do carro: " + carro.getModelo());
+		ConnectionFactory cf = new ConnectionFactory();
+		cf.getConexao();
 	}
 
 }
