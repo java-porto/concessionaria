b92c539263f94c958c2800403fda046c1e0de2d6
feliperodalv <felipe.rodrigues.alves10@gmail.com>
2024-10-22 16:17:06 -0300

    Commit

--- Loja01/src/main/java/br/com/concessionaria/factory/ConnectionFactory.java
+++ Loja01/src/main/java/br/com/concessionaria/factory/ConnectionFactory.java
@@ -1,13 +1,32 @@
 package br.com.concessionaria.factory;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
-	public static final Connection conexao = null;
+	public static Connection conexao = null;
 	public static final String URL = "jdbc:mysql://192.168.4228:3306/concessionaria";
+	public static final String USUARIO = "root";
+	public static final String SENHA = "root";
 	
-	public ConnectionFactory() {
-		
+	public Connection getConexao() {
+		try {
+			//Carregar o drive do JDBC do MYSQL
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//Estabelecer a conexão com o BANCO DE DADOS
+			conexao = DriverManager.getConnection(URL,USUARIO,SENHA);
+			System.out.println("CONEXÃO ESTABELECIDA COM SUCESSO!");
+			
+		}catch(ClassNotFoundException e) {
+			System.out.println("Drive JDBC não encontrado!");
+			e.printStackTrace();
+		}catch (SQLException e) {
+			System.out.println("Erro ao conectar com o Banco!");
+			e.printStackTrace();
+		}
+		return conexao;
 	}
 }
--- Loja01/src/main/java/br/com/concessionaria/teste/Teste.java
+++ Loja01/src/main/java/br/com/concessionaria/teste/Teste.java
@@ -1,16 +1,23 @@
 package br.com.concessionaria.teste;
 
 import br.com.concessionaria.dao.CarroDAO;
+import br.com.concessionaria.factory.ConnectionFactory;
 import br.com.concessionaria.model.Carro;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		CarroDAO cDAO = new CarroDAO();
-		Carro carro = cDAO.select(3);
 		
-		System.out.println("nome do carro: " + carro.getNome());
-		System.out.println("Modelo do carro: " + carro.getModelo());
+		//CarroDAO cDAO = new CarroDAO();
+		//Carro carro = cDAO.select(3);
+		
+		//System.out.println("nome do carro: " + carro.getNome());
+		//System.out.println("Modelo do carro: " + carro.getModelo());
+	
+		ConnectionFactory cf = new ConnectionFactory();
+		cf.getConexao();
+		
+	
 	}
 
 }
